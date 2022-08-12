59af3e5dc8ada7457beef03c5218649cfad45b9a
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2022-08-12 14:42:30 +0200

    adding Specialities to data loader and superclass

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/owners")
 public class OwnerController {
 
     private final OwnerService ownerService;
@@ -14,7 +15,7 @@ public class OwnerController {
         this.ownerService = ownerService;
     }
 
-    @RequestMapping({"/owners/","/owners","/owners/index","/owners/index/html"})
+    @RequestMapping({"/","","/index","/index.html"})
     public String listOwners(Model model){
 
         model.addAttribute("owners", ownerService.findAll());
@@ -22,4 +23,8 @@ public class OwnerController {
         return "owners/index";
     }
 
+    @RequestMapping("/find")
+    public String findOwners(){
+        return "notImplemented";
+    }
 }
